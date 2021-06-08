3e15c08923ab9d87bd09d31e3835c35756b868ed
Solomon <smason19@emich.edu>
2021-06-08 15:09:10 -0400

    'Third_Project'

--- TornadoEvents.py
+++ TornadoEvents.py
@@ -14,14 +14,6 @@ file = "MichiganTornados.csv"
 
 tempData = pd.read_csv(file, error_bad_lines=False)
 
-#tempData['MAGNITUDE'] = tempData['MAGNITUDE'].replace(r'^\s*$', np.nan, regex=True)
-#tempData['MAGNITUDE'] = tempData['MAGNITUDE'].astype(float)
-
-#tempData['BEGIN_TIME'].replace(0, np.nan)
-#tempData['BEGIN_TIME'] = tempData['BEGIN_TIME'].astype(str)
-#tempData['BEGIN_TIME'] = tempData['BEGIN_TIME'].str.zfill(4)
-
-#tempData = tempData.dropna()
 tempData.drop(labels = 0, axis = 0, inplace = True)
 
 tempData['Year'] = pd.to_datetime(tempData['Year'], format = '%Y')
@@ -31,11 +23,8 @@ tempData['# of Tornados'] = tempData['# of Tornados'].astype(float)
 
 data = pd.DataFrame(tempData[['Year','# of Tornados',]])
 
-#data = data.assign(newtime=pd.to_datetime(data.pop('BEGIN_TIME'), format='%H%M').dt.strftime('%H:%M'))
-#data.newtime = pd.to_timedelta(data.newtime+':00')
-#data = data.assign(observe_time = data.pop('BEGIN_DATE') + data.pop('newtime'))
-
 data.plot('Year', '# of Tornados', figsize = (10,5))
+
 
 
 
